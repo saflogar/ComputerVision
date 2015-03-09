dc9ff253eddf9494e705112766ceac1a3000209a
Sergio Flores <safgflores@hotmail.com>
2015-03-09 03:19:18 -0700

    added median filter and multiple mask support for matching template

--- border.py
+++ border.py
@@ -12,30 +12,41 @@ def readImage(imageName):
     im = array(Image.open(imageName).convert('L'))
     return im
 
-def medianFilter():
-    im = readImage('empire.jpg')
+def medianFilter(imString):    
+    im = readImage(imString)
     lx = len(im)
     ly = len (im[0])
     mLen = 3
     mWid = 3
-
     newIm = Image.new("RGB",(ly,lx),"white")
     newImArr = arange(im.size,dtype=np.uint8).reshape(lx,ly)    
     print "[DEBUG] lx = ",lx, "ly = ",ly                
     for x in range (lx-1):
-        for y in range (ly-1):               
-            hist = [0 for i in range(255)]
+        for y in range (ly-1):
+            hist = [0 for i in range(256)]
+            neighList = [0 for i in range(mLen)]
             for i in range (mLen):
                 suma = 0
                 for j in range (mWid):
                     ii = (x-1)+i
-                    jj = (y-1)+j         
+                    jj = (y-1)+j
                     if ii >= 0 and jj >= 0 and jj < ly and ii < lx:
                         hist[im[ii][jj]] += 1
-            sorHist = bubbleSort(hist) 
-            pdb.set_trace()
-def bubbleSort(sortList):
-    
+                        neighList.append(im[ii][jj])
+#                        neighList[]                   
+            sorHist = bubbleSort(neighList)
+#           print sorHist
+            newImArr[x][y] = sorHist[4]
+#           pdb.set_trace()
+#           newIm = Image
+#           pdb.set_trace()
+    newIm = Image.fromarray(newImArr,'L')
+
+    test = Image.fromarray(im,'L')
+    test.save("test.jpg")
+    newIm.save("median.jpg")    
+
+def bubbleSort(sortList):    
     cont = 1
     while cont != 0:        
         cont = 0
@@ -46,55 +57,59 @@ def bubbleSort(sortList):
                 sortList[i]  = b
                 sortList[i+1]= a
                 cont = 1
+#    print sortList
     return sortList
-        
-
-            
-            
-           
-def convelve (mask):
-    im = readImage('convelved.jpg')
+                                       
+def convelve (mask,imString):
+    im = readImage(imString)
 #    mask = [[0,-1,0],[-1,5,-1],[0,-1,0]]
 #    mask = [[-1,-1,-1],[-1,8,-1],[-1,-1,-1]]
-#    mask = [[1,1,1],[1,1,1],[1,1,1]] Gaussian *Remember divide between 9 each 
+#    mask = [[1,1,1],[1,1,1],[1,1,1]] #Gaussian *Remember divide between 9 each 
 #    pdb.set_trace()
+
     lx = len(im)
     ly = len (im[0])
-    mLen = len(mask)
-    mWid = len(mask[0])
-    mask.reverse()
+    mLen = len(mask[0])
+    mWid = len(mask[0][0])
+  
     newIm = Image.new("RGB",(ly,lx),"white")
     newImArr = arange(im.size,dtype=np.uint8).reshape(lx,ly)
     print "[DEBUG] lx = ",lx, "ly = ",ly
- #   pdb.set_trace()
+
     for x in range (lx-1):
         for y in range (ly-1):
             total = 0   
             #newImArr[x][y] = im[x][y]
-            for i in range (mLen):
-                suma = 0
-                for j in range (mWid):
-                    ii = (x-1)+i
-                    jj = (y-1)+j         
-                    if ii >= 0 and jj >= 0 and jj < ly and ii < lx:
-                        suma = im[ii][jj] * mask[i][j] 
-                        #print "total[",x,"]""[",y,"] = im[",ii,"][",jj,"] * mask[",i,"][",j,"]=",suma
-                        total += suma                     
-                        
- #           print total ,"in [",x,"][",y,"]"
-#            print "x=",x,"y=",y
-            newImArr[x][y] = total
-#    pdb.set_trace()
+            v = 0
+            pixelGrads = [0 for i in range(8)]
+            for m in mask:
+                m.reverse()                
+                for i in range (mLen):
+                    suma = 0
+                    for j in range (mWid):
+                        ii = (x-1)+i
+                        jj = (y-1)+j         
+                        if ii >= 0 and jj >= 0 and jj < ly and ii < lx:
+                        #suma = im[ii][jj] * (mask[i][j] / float(9) )
+                            suma = im[ii][jj] * m[i][j]
+                            pixelGrads[v] = pixelGrads[v]+ suma
+                            total += suma   
+                v=v+1
+                newImArr[x][y] = max(pixelGrads)
+            
+            
     newIm = Image.fromarray(newImArr,'L')
     test = Image.fromarray(im,'L')
     test.save("test.jpg")
-    newIm.save("convelved.jpg")
-
-#masks = [[-1,1,1],[-1,-2,1],[-1,1,1]],[[1,1,1],[1,-2,1],[-1,-1,1]],[[1,1,1],[1,-2,1],[-1,-1,-1]],[[1,1,1],[1,-2,-1],[1,-1,-1]]
-#masks[1] = [[1,1,1],[1,-2,1],[-1,-1,1]]
-#masks[2] = [[1,1,1],[1,-2,1],[-1,-1,-1]]
-#masks[3] = [[1,1,1],[1,-2,-1],[1,-1,-1]]
-#pdb.set_trace()
+    newIm.save("gaussianConv.jpg")
+masks = [[-1,1,1],[-1,-2,1],[-1,1,1]],[[1,1,1],[1,-2,1],[-1,-1,1]],[[1,1,1],[1,-2,1],[-1,-1,-1]],[[1,1,1],[1,-2,-1],[1,-1,-1]]
 #for i in range(len(masks)):
-#convelve(masks[3])        
-medianFilter()
+#mask = [[1,1,1],[1,1,1],[1,1,1]]#gaussian
+mask = [[-1,-2,-1],[0,0,0],[1,2,1]]
+#medianFilter('median.jpg')
+
+medianFilter('empire.jpg')
+convelve(masks,'median.jpg')
+#convelve(masks[3],'empire.jpg')
+        
+
